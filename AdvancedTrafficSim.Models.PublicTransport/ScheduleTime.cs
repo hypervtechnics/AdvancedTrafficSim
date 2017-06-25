5be63124c31f0fbdb25d15bdce76657b4f03ab86
Johannes <j.fusenig@gmx.net>
2017-06-25 23:56:07 +0200

    Added first Classes

---
+++ AdvancedTrafficSim.Models.PublicTransport/ScheduleTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTrafficSim.Models.PublicTransport
+{
+    public class ScheduleTime
+    {
+        private int day, hour, minute;
+
+        public ScheduleTime(int day, int hour, int minute)
+        {
+            Day = day;
+            Hour = hour;
+            Minute = minute;
+        }
+
+        public int Day { get => day; set => day = value; }
+        public int Hour { get => hour; set => hour = value; }
+        public int Minute { get => minute; set => minute = value; }
+    }
+}
